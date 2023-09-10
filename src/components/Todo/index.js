0f4848363ff30fa232e03edd2a5d7770fafb2a64
Truong Cong Hieu <truongconghieuptit@gmail.com>
2023-09-10 16:30:53 +0700

    do homework of lesson 5

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,13 +1,42 @@
+import { useState } from "react";
+
 import "./style.css";
 
-const Todo = ({ todo }) => {
-  const { text, isCompleted } = todo;
+const Todo = ({ todo, handleChangeStatus, editTodo }) => {
+  const { isCompleted, id } = todo;
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState(todo.text);
+
   const todoTextClass = `todo__text ${isCompleted && "todo__text--completed"}`;
+  const handleEditTodo = (event) => {
+    if (event.key === "Enter" && text) {
+      editTodo(id, text);
+      setIsEditing(!isEditing);
+    }
+  };
 
   return (
     <div className="todo">
-      <input type="radio" checked={isCompleted}></input>
-      <label className={todoTextClass}>{text}</label>
+      <input
+        type="checkbox"
+        checked={isCompleted}
+        onChange={() => handleChangeStatus(id)}
+      ></input>
+      {isEditing ? (
+        <input
+          type="text"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          onKeyDown={handleEditTodo}
+        ></input>
+      ) : (
+        <label
+          className={todoTextClass}
+          onDoubleClick={() => setIsEditing(!isEditing)}
+        >
+          {text}
+        </label>
+      )}
     </div>
   );
 };
